0f9fc72a66a1755c1d13503dd5942694fe8da051
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2025-12-05 00:26:52 +0900

    feat: asset 추가 항목 적용

--- src/constant/items.ts
+++ src/constant/items.ts
@@ -1,59 +1,207 @@
-import hairTwin from '../assets/image/hair_twin.png'
-import body2 from '../assets/image/body_2.png'
+// This file is auto-generated. Do not edit manually.
+
+// Blush Imports
+import blush_1 from '@/assets/image/blush/blush_1.png';
+import blush_2 from '@/assets/image/blush/blush_2.png';
+import blush_3 from '@/assets/image/blush/blush_3.png';
+import blush_4 from '@/assets/image/blush/blush_4.png';
+import blush_5 from '@/assets/image/blush/blush_5.png';
+
+// Body Imports
+import body_1 from '@/assets/image/body/body_1.png';
+import body_2 from '@/assets/image/body/body_2.png';
+import body_3 from '@/assets/image/body/body_3.png';
+import body_4 from '@/assets/image/body/body_4.png';
+import body_5 from '@/assets/image/body/body_5.png';
+import body_6 from '@/assets/image/body/body_6.png';
+import body_7 from '@/assets/image/body/body_7.png';
+import body_8 from '@/assets/image/body/body_8.png';
+import body_9 from '@/assets/image/body/body_9.png';
+import body_10 from '@/assets/image/body/body_10.png';
+
+// Eyes Imports
+import eye_1 from '@/assets/image/eyes/eye_1.png';
+import eye_2 from '@/assets/image/eyes/eye_2.png';
+import eye_3 from '@/assets/image/eyes/eye_3.png';
+import eye_4 from '@/assets/image/eyes/eye_4.png';
+import eye_5 from '@/assets/image/eyes/eye_5.png';
+import eye_6 from '@/assets/image/eyes/eye_6.png';
+import eye_7 from '@/assets/image/eyes/eye_7.png';
+import eye_8 from '@/assets/image/eyes/eye_8.png';
+import eye_9 from '@/assets/image/eyes/eye_9.png';
+import eye_10 from '@/assets/image/eyes/eye_10.png';
+import eye_11 from '@/assets/image/eyes/eye_11.png';
+import eye_12 from '@/assets/image/eyes/eye_12.png';
+import eye_13 from '@/assets/image/eyes/eye_13.png';
+import eye_14 from '@/assets/image/eyes/eye_14.png';
+import eye_15 from '@/assets/image/eyes/eye_15.png';
+
+// Hair Imports
+import hair_1 from '@/assets/image/hair/hair_1.png';
+import hair_2 from '@/assets/image/hair/hair_2.png';
+import hair_3 from '@/assets/image/hair/hair_3.png';
+import hair_4 from '@/assets/image/hair/hair_4.png';
+import hair_5 from '@/assets/image/hair/hair_5.png';
+import hair_6 from '@/assets/image/hair/hair_6.png';
+import hair_7 from '@/assets/image/hair/hair_7.png';
+
+// Hat Imports
+import hat_1 from '@/assets/image/hat/hat_1.png';
+import hat_2 from '@/assets/image/hat/hat_2.png';
+import hat_3 from '@/assets/image/hat/hat_3.png';
+import hat_4 from '@/assets/image/hat/hat_4.png';
+import hat_5 from '@/assets/image/hat/hat_5.png';
+import hat_6 from '@/assets/image/hat/hat_6.png';
+import hat_7 from '@/assets/image/hat/hat_7.png';
+import hat_8 from '@/assets/image/hat/hat_8.png';
+
+// Mouth Imports
+import mouth_1 from '@/assets/image/mouth/mouth_1.png';
+import mouth_2 from '@/assets/image/mouth/mouth_2.png';
+import mouth_3 from '@/assets/image/mouth/mouth_3.png';
+import mouth_4 from '@/assets/image/mouth/mouth_4.png';
+import mouth_5 from '@/assets/image/mouth/mouth_5.png';
+import mouth_6 from '@/assets/image/mouth/mouth_6.png';
+import mouth_7 from '@/assets/image/mouth/mouth_7.png';
+import mouth_8 from '@/assets/image/mouth/mouth_8.png';
+import mouth_9 from '@/assets/image/mouth/mouth_9.png';
+import mouth_10 from '@/assets/image/mouth/mouth_10.png';
+import mouth_11 from '@/assets/image/mouth/mouth_11.png';
+import mouth_12 from '@/assets/image/mouth/mouth_12.png';
+import mouth_13 from '@/assets/image/mouth/mouth_13.png';
+import mouth_14 from '@/assets/image/mouth/mouth_14.png';
+import mouth_15 from '@/assets/image/mouth/mouth_15.png';
+import mouth_16 from '@/assets/image/mouth/mouth_16.png';
+import mouth_17 from '@/assets/image/mouth/mouth_17.png';
+import mouth_18 from '@/assets/image/mouth/mouth_18.png';
+import mouth_19 from '@/assets/image/mouth/mouth_19.png';
+import mouth_20 from '@/assets/image/mouth/mouth_20.png';
+
+// OnePiece Imports
+import onePiece_1 from '@/assets/image/onePiece/onePiece_1.png';
+import onePiece_2 from '@/assets/image/onePiece/onePiece_2.png';
+import onePiece_3 from '@/assets/image/onePiece/onePiece_3.png';
+import onePiece_4 from '@/assets/image/onePiece/onePiece_4.png';
+
+// Pants Imports
+import pants_1 from '@/assets/image/pants/pants_1.png';
+import pants_1_1 from '@/assets/image/pants/pants_1-1.png';
+import pants_1_2 from '@/assets/image/pants/pants_1-2.png';
+import pants_4 from '@/assets/image/pants/pants_4.png';
+import pants_5 from '@/assets/image/pants/pants_5.png';
+
+
+// Top Imports
+import top_1 from '@/assets/image/top/top_1.png';
+import top_2 from '@/assets/image/top/top_2.png';
+import top_3 from '@/assets/image/top/top_3.png';
+import top_4 from '@/assets/image/top/top_4.png';
+import top_5 from '@/assets/image/top/top_5.png';
+import top_6 from '@/assets/image/top/top_6.png';
+import top_7 from '@/assets/image/top/top_7.png';
 
 export const itemsData = {
   face: {
-    default: [
-      { name: 'Face 1', asset: 'https://via.placeholder.com/100/FF0000' },
-      { name: 'Face 2', asset: 'https://via.placeholder.com/100/00FF00' },
-      { name: 'Face 3', asset: 'https://via.placeholder.com/100/0000FF' },
-      { name: 'Face 4', asset: 'https://via.placeholder.com/100/FFFF00' },
-      { name: 'Face 5', asset: 'https://via.placeholder.com/100/FF00FF' },
-      { name: 'Face 6', asset: 'https://via.placeholder.com/100/00FFFF' },
-      { name: 'Face 7', asset: 'https://via.placeholder.com/100/FFA500' },
-      { name: 'Face 8', asset: 'https://via.placeholder.com/100/800080' },
-      { name: 'Face 9', asset: 'https://via.placeholder.com/100/FFC0CB' },
-      { name: 'Face 10', asset: 'https://via.placeholder.com/100/D3D3D3' },
+    body: [
+      { name: 'body_1', asset: body_1 },
+      { name: 'body_2', asset: body_2 },
+      { name: 'body_3', asset: body_3 },
+      { name: 'body_4', asset: body_4 },
+      { name: 'body_5', asset: body_5 },
+      { name: 'body_6', asset: body_6 },
+      { name: 'body_7', asset: body_7 },
+      { name: 'body_8', asset: body_8 },
+      { name: 'body_9', asset: body_9 },
+      { name: 'body_10', asset: body_10 },
+    ],
+    eyes: [
+        { name: 'eye_1', asset: eye_1 },
+        { name: 'eye_2', asset: eye_2 },
+        { name: 'eye_3', asset: eye_3 },
+        { name: 'eye_4', asset: eye_4 },
+        { name: 'eye_5', asset: eye_5 },
+        { name: 'eye_6', asset: eye_6 },
+        { name: 'eye_7', asset: eye_7 },
+        { name: 'eye_8', asset: eye_8 },
+        { name: 'eye_9', asset: eye_9 },
+        { name: 'eye_10', asset: eye_10 },
+        { name: 'eye_11', asset: eye_11 },
+        { name: 'eye_12', asset: eye_12 },
+        { name: 'eye_13', asset: eye_13 },
+        { name: 'eye_14', asset: eye_14 },
+        { name: 'eye_15', asset: eye_15 },
+    ],
+    hair: [
+        { name: 'hair_1', asset: hair_1 },
+        { name: 'hair_2', asset: hair_2 },
+        { name: 'hair_3', asset: hair_3 },
+        { name: 'hair_4', asset: hair_4 },
+        { name: 'hair_5', asset: hair_5 },
+        { name: 'hair_6', asset: hair_6 },
+        { name: 'hair_7', asset: hair_7 },
+    ],
+    mouth: [
+        { name: 'mouth_1', asset: mouth_1 },
+        { name: 'mouth_2', asset: mouth_2 },
+        { name: 'mouth_3', asset: mouth_3 },
+        { name: 'mouth_4', asset: mouth_4 },
+        { name: 'mouth_5', asset: mouth_5 },
+        { name: 'mouth_6', asset: mouth_6 },
+        { name: 'mouth_7', asset: mouth_7 },
+        { name: 'mouth_8', asset: mouth_8 },
+        { name: 'mouth_9', asset: mouth_9 },
+        { name: 'mouth_10', asset: mouth_10 },
+        { name: 'mouth_11', asset: mouth_11 },
+        { name: 'mouth_12', asset: mouth_12 },
+        { name: 'mouth_13', asset: mouth_13 },
+        { name: 'mouth_14', asset: mouth_14 },
+        { name: 'mouth_15', asset: mouth_15 },
+        { name: 'mouth_16', asset: mouth_16 },
+        { name: 'mouth_17', asset: mouth_17 },
+        { name: 'mouth_18', asset: mouth_18 },
+        { name: 'mouth_19', asset: mouth_19 },
+        { name: 'mouth_20', asset: mouth_20 },
+    ],
+    blush: [
+        { name: 'blush_1', asset: blush_1 },
+        { name: 'blush_2', asset: blush_2 },
+        { name: 'blush_3', asset: blush_3 },
+        { name: 'blush_4', asset: blush_4 },
+        { name: 'blush_5', asset: blush_5 },
     ],
   },
   clothes: {
     hat: [
-      { name: 'Hat 1', asset: hairTwin },
-      { name: 'Hat 2', asset: 'https://via.placeholder.com/100/0000FF' },
-      { name: 'Hat 3', asset: 'https://via.placeholder.com/100/FF0000' },
-      { name: 'Hat 4', asset: 'https://via.placeholder.com/100/00FF00' },
-      { name: 'Hat 5', asset: 'https://via.placeholder.com/100/FFFF00' },
-      { name: 'Hat 6', asset: 'https://via.placeholder.com/100/FF00FF' },
-      { name: 'Hat 7', asset: 'https://via.placeholder.com/100/00FFFF' },
-      { name: 'Hat 8', asset: 'https://via.placeholder.com/100/FFA500' },
-      { name: 'Hat 9', asset: 'https://via.placeholder.com/100/800080' },
-      { name: 'Hat 10', asset: 'https://via.placeholder.com/100/FFC0CB' },
+        { name: 'hat_1', asset: hat_1 },
+        { name: 'hat_2', asset: hat_2 },
+        { name: 'hat_3', asset: hat_3 },
+        { name: 'hat_4', asset: hat_4 },
+        { name: 'hat_5', asset: hat_5 },
+        { name: 'hat_6', asset: hat_6 },
+        { name: 'hat_7', asset: hat_7 },
+        { name: 'hat_8', asset: hat_8 },
     ],
-    outfit: [
-      { name: 'Outfit 1', asset: body2 },
-      { name: 'Outfit 2', asset: 'https://via.placeholder.com/100/FFFF00' },
-      { name: 'Outfit 3', asset: 'https://via.placeholder.com/100/FF00FF' },
-      { name: 'Outfit 4', asset: 'https://via.placeholder.com/100/00FFFF' },
-      { name: 'Outfit 5', asset: 'https://via.placeholder.com/100/FFA500' },
-      { name: 'Outfit 6', asset: 'https://via.placeholder.com/100/800080' },
-      { name: 'Outfit 7', asset: 'https://via.placeholder.com/100/FF0000' },
-      { name: 'Outfit 8', asset: 'https://via.placeholder.com/100/00FF00' },
-      { name: 'Outfit 9', asset: 'https://via.placeholder.com.com/100/FFFF00' },
-      { name: 'Outfit 10', asset: 'https://via.placeholder.com/100/FF00FF' },
+    onePiece: [
+        { name: 'onePiece_1', asset: onePiece_1 },
+        { name: 'onePiece_2', asset: onePiece_2 },
+        { name: 'onePiece_3', asset: onePiece_3 },
+        { name: 'onePiece_4', asset: onePiece_4 },
     ],
-  },
-  decorations: {
-    default: [
-      { name: 'Deco 1', asset: 'https://via.placeholder.com/100/FFA500' },
-      { name: 'Deco 2', asset: 'https://via.placeholder.com/100/800080' },
-      { name: 'Deco 3', asset: 'https://via.placeholder.com/100/FF0000' },
-      { name: 'Deco 4', asset: 'https://via.placeholder.com/100/00FF00' },
-      { name: 'Deco 5', asset: 'https://via.placeholder.com/100/FFFF00' },
-      { name: 'Deco 6', asset: 'https://via.placeholder.com/100/FF00FF' },
-      { name: 'Deco 7', asset: 'https://via.placeholder.com/100/00FFFF' },
-      { name: 'Deco 8', asset: 'https://via.placeholder.com/100/FFA500' },
-      { name: 'Deco 9', asset: 'https://via.placeholder.com/100/800080' },
-      { name: 'Deco 10', asset: 'https://via.placeholder.com/100/FFC0CB' },
+    pants: [
+        { name: 'pants_1', asset: pants_1 },
+        { name: 'pants_1_1', asset: pants_1_1 },
+        { name: 'pants_1_2', asset: pants_1_2 },
+        { name: 'pants_4', asset: pants_4 },
+        { name: 'pants_5', asset: pants_5 },
+    ],
+    top: [
+        { name: 'top_1', asset: top_1 },
+        { name: 'top_2', asset: top_2 },
+        { name: 'top_3', asset: top_3 },
+        { name: 'top_4', asset: top_4 },
+        { name: 'top_5', asset: top_5 },
+        { name: 'top_6', asset: top_6 },
+        { name: 'top_7', asset: top_7 },
     ],
   },
-}
+};
