16ff13d29639b4f885c98c29bf3eada8c1cbbeef
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2025-12-05 14:56:22 +0900

    feat: step2 route 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,5 @@
 import Step1MakeCookie from '@/pages/cookie/Step1MakeCookie'
+import Step2ChooseReceiver from '@/pages/cookie/Step2ChooseReceiver'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 const Router = () => {
@@ -9,6 +10,7 @@ const Router = () => {
           {/* /cookie로 들어오면 /cookie/step1로 리다이렉트 */}
           <Route index element={<Navigate to="step1" replace />} />
           <Route path="step1" element={<Step1MakeCookie />} />
+          <Route path="step2" element={<Step2ChooseReceiver />} />
         </Route>
       </Routes>
     </BrowserRouter>
