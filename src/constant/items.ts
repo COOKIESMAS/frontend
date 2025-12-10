ca14e906e426044986ab872b780315a78bf81f6c
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2025-12-10 22:45:16 +0900

    minor: 에셋 변경 적용

--- src/constant/items.ts
+++ src/constant/items.ts
@@ -1,120 +1,193 @@
-// This file is auto-generated. Do not edit manually.
+// Accessory Imports
+import accessory_1 from '@/assets/image/accessory/accessory_1.svg'
+import accessory_2 from '@/assets/image/accessory/accessory_2.svg'
 
 // Blush Imports
-import blush_1 from '@/assets/image/blush/blush_1.png';
-import blush_2 from '@/assets/image/blush/blush_2.png';
-import blush_3 from '@/assets/image/blush/blush_3.png';
-import blush_4 from '@/assets/image/blush/blush_4.png';
-import blush_5 from '@/assets/image/blush/blush_5.png';
+import blush_1 from '@/assets/image/blush/blush_1.svg'
+import blush_2 from '@/assets/image/blush/blush_2.svg'
+import blush_3 from '@/assets/image/blush/blush_3.svg'
+import blush_4 from '@/assets/image/blush/blush_4.svg'
+import blush_5 from '@/assets/image/blush/blush_5.svg'
 
 // Body Imports
-import body_1 from '@/assets/image/body/body_1.png';
-import body_2 from '@/assets/image/body/body_2.png';
-import body_3 from '@/assets/image/body/body_3.png';
-import body_4 from '@/assets/image/body/body_4.png';
-import body_5 from '@/assets/image/body/body_5.png';
-import body_6 from '@/assets/image/body/body_6.png';
-import body_7 from '@/assets/image/body/body_7.png';
-import body_8 from '@/assets/image/body/body_8.png';
-import body_9 from '@/assets/image/body/body_9.png';
-import body_10 from '@/assets/image/body/body_10.png';
+import body_1 from '@/assets/image/body/body_1.svg'
+import body_2 from '@/assets/image/body/body_2.svg'
+import body_3 from '@/assets/image/body/body_3.svg'
+import body_4 from '@/assets/image/body/body_4.svg'
+import body_5 from '@/assets/image/body/body_5.svg'
+import body_6 from '@/assets/image/body/body_6.svg'
+import body_7 from '@/assets/image/body/body_7.svg'
+import body_8 from '@/assets/image/body/body_8.svg'
+import body_9 from '@/assets/image/body/body_9.svg'
+import body_10 from '@/assets/image/body/body_10.svg'
 
 // Eyes Imports
-import eye_1 from '@/assets/image/eyes/eye_1.png';
-import eye_2 from '@/assets/image/eyes/eye_2.png';
-import eye_3 from '@/assets/image/eyes/eye_3.png';
-import eye_4 from '@/assets/image/eyes/eye_4.png';
-import eye_5 from '@/assets/image/eyes/eye_5.png';
-import eye_6 from '@/assets/image/eyes/eye_6.png';
-import eye_7 from '@/assets/image/eyes/eye_7.png';
-import eye_8 from '@/assets/image/eyes/eye_8.png';
-import eye_9 from '@/assets/image/eyes/eye_9.png';
-import eye_10 from '@/assets/image/eyes/eye_10.png';
-import eye_11 from '@/assets/image/eyes/eye_11.png';
-import eye_12 from '@/assets/image/eyes/eye_12.png';
-import eye_13 from '@/assets/image/eyes/eye_13.png';
-import eye_14 from '@/assets/image/eyes/eye_14.png';
-import eye_15 from '@/assets/image/eyes/eye_15.png';
+import eye_1 from '@/assets/image/eyes/eye_1.svg'
+import eye_2 from '@/assets/image/eyes/eye_2.svg'
+import eye_3 from '@/assets/image/eyes/eye_3.svg'
+import eye_4 from '@/assets/image/eyes/eye_4.svg'
+import eye_5 from '@/assets/image/eyes/eye_5.svg'
+import eye_6 from '@/assets/image/eyes/eye_6.svg'
+import eye_7 from '@/assets/image/eyes/eye_7.svg'
+import eye_8 from '@/assets/image/eyes/eye_8.svg'
+import eye_9 from '@/assets/image/eyes/eye_9.svg'
+import eye_10 from '@/assets/image/eyes/eye_10.svg'
+import eye_11 from '@/assets/image/eyes/eye_11.svg'
+import eye_12 from '@/assets/image/eyes/eye_12.svg'
+import eye_13 from '@/assets/image/eyes/eye_13.svg'
+import eye_14 from '@/assets/image/eyes/eye_14.svg'
+import eye_15 from '@/assets/image/eyes/eye_15.svg'
 
 // Hair Imports
-import hair_1 from '@/assets/image/hair/hair_1.png';
-import hair_2 from '@/assets/image/hair/hair_2.png';
-import hair_3 from '@/assets/image/hair/hair_3.png';
-import hair_4 from '@/assets/image/hair/hair_4.png';
-import hair_5 from '@/assets/image/hair/hair_5.png';
-import hair_6 from '@/assets/image/hair/hair_6.png';
-import hair_7 from '@/assets/image/hair/hair_7.png';
+import hair_1 from '@/assets/image/hair/hair_1.svg'
+import hair_2 from '@/assets/image/hair/hair_2.svg'
+import hair_3 from '@/assets/image/hair/hair_3.svg'
+import hair_4 from '@/assets/image/hair/hair_4.svg'
+import hair_5 from '@/assets/image/hair/hair_5.svg'
+import hair_6 from '@/assets/image/hair/hair_6.svg'
+import hair_7 from '@/assets/image/hair/hair_7.svg'
 
 // Hat Imports
-import hat_1 from '@/assets/image/hat/hat_1.png';
-import hat_2 from '@/assets/image/hat/hat_2.png';
-import hat_3 from '@/assets/image/hat/hat_3.png';
-import hat_4 from '@/assets/image/hat/hat_4.png';
-import hat_5 from '@/assets/image/hat/hat_5.png';
-import hat_6 from '@/assets/image/hat/hat_6.png';
-import hat_7 from '@/assets/image/hat/hat_7.png';
-import hat_8 from '@/assets/image/hat/hat_8.png';
+import hat_1 from '@/assets/image/hat/hat_1.svg'
+import hat_2 from '@/assets/image/hat/hat_2.svg'
+import hat_3 from '@/assets/image/hat/hat_3.svg'
+import hat_4 from '@/assets/image/hat/hat_4.svg'
+import hat_5 from '@/assets/image/hat/hat_5.svg'
+import hat_6 from '@/assets/image/hat/hat_6.svg'
+import hat_7 from '@/assets/image/hat/hat_7.svg'
+import hat_8 from '@/assets/image/hat/hat_8.svg'
 
 // Mouth Imports
-import mouth_1 from '@/assets/image/mouth/mouth_1.png';
-import mouth_2 from '@/assets/image/mouth/mouth_2.png';
-import mouth_3 from '@/assets/image/mouth/mouth_3.png';
-import mouth_4 from '@/assets/image/mouth/mouth_4.png';
-import mouth_5 from '@/assets/image/mouth/mouth_5.png';
-import mouth_6 from '@/assets/image/mouth/mouth_6.png';
-import mouth_7 from '@/assets/image/mouth/mouth_7.png';
-import mouth_8 from '@/assets/image/mouth/mouth_8.png';
-import mouth_9 from '@/assets/image/mouth/mouth_9.png';
-import mouth_10 from '@/assets/image/mouth/mouth_10.png';
-import mouth_11 from '@/assets/image/mouth/mouth_11.png';
-import mouth_12 from '@/assets/image/mouth/mouth_12.png';
-import mouth_13 from '@/assets/image/mouth/mouth_13.png';
-import mouth_14 from '@/assets/image/mouth/mouth_14.png';
-import mouth_15 from '@/assets/image/mouth/mouth_15.png';
-import mouth_16 from '@/assets/image/mouth/mouth_16.png';
-import mouth_17 from '@/assets/image/mouth/mouth_17.png';
-import mouth_18 from '@/assets/image/mouth/mouth_18.png';
-import mouth_19 from '@/assets/image/mouth/mouth_19.png';
-import mouth_20 from '@/assets/image/mouth/mouth_20.png';
+import mouth_1 from '@/assets/image/mouth/mouth_1.svg'
+import mouth_2 from '@/assets/image/mouth/mouth_2.svg'
+import mouth_3 from '@/assets/image/mouth/mouth_3.svg'
+import mouth_4 from '@/assets/image/mouth/mouth_4.svg'
+import mouth_5 from '@/assets/image/mouth/mouth_5.svg'
+import mouth_6 from '@/assets/image/mouth/mouth_6.svg'
+import mouth_7 from '@/assets/image/mouth/mouth_7.svg'
+import mouth_8 from '@/assets/image/mouth/mouth_8.svg'
+import mouth_9 from '@/assets/image/mouth/mouth_9.svg'
+import mouth_10 from '@/assets/image/mouth/mouth_10.svg'
+import mouth_11 from '@/assets/image/mouth/mouth_11.svg'
+import mouth_12 from '@/assets/image/mouth/mouth_12.svg'
+import mouth_13 from '@/assets/image/mouth/mouth_13.svg'
+import mouth_14 from '@/assets/image/mouth/mouth_14.svg'
+import mouth_15 from '@/assets/image/mouth/mouth_15.svg'
+import mouth_16 from '@/assets/image/mouth/mouth_16.svg'
+import mouth_17 from '@/assets/image/mouth/mouth_17.svg'
+import mouth_18 from '@/assets/image/mouth/mouth_18.svg'
+import mouth_19 from '@/assets/image/mouth/mouth_19.svg'
+import mouth_20 from '@/assets/image/mouth/mouth_20.svg'
 
 // OnePiece Imports
-import onePiece_1 from '@/assets/image/onePiece/onePiece_1.png';
-import onePiece_2 from '@/assets/image/onePiece/onePiece_2.png';
-import onePiece_3 from '@/assets/image/onePiece/onePiece_3.png';
-import onePiece_4 from '@/assets/image/onePiece/onePiece_4.png';
+import onePiece_1 from '@/assets/image/onePiece/onePiece_1.svg'
+import onePiece_2 from '@/assets/image/onePiece/onePiece_2.svg'
+import onePiece_3 from '@/assets/image/onePiece/onePiece_3.svg'
+import onePiece_4 from '@/assets/image/onePiece/onePiece_4.svg'
 
 // Pants Imports
-import pants_1 from '@/assets/image/pants/pants_1.png';
-import pants_1_1 from '@/assets/image/pants/pants_1-1.png';
-import pants_1_2 from '@/assets/image/pants/pants_1-2.png';
-import pants_4 from '@/assets/image/pants/pants_4.png';
-import pants_5 from '@/assets/image/pants/pants_5.png';
-
+import pants_1 from '@/assets/image/pants/pants_1.svg'
+import pants_2 from '@/assets/image/pants/pants_2.svg'
+import pants_3 from '@/assets/image/pants/pants_3.svg'
+import pants_4 from '@/assets/image/pants/pants_4.svg'
+import pants_5 from '@/assets/image/pants/pants_5.svg'
 
 // Top Imports
-import top_1 from '@/assets/image/top/top_1.png';
-import top_2 from '@/assets/image/top/top_2.png';
-import top_3 from '@/assets/image/top/top_3.png';
-import top_4 from '@/assets/image/top/top_4.png';
-import top_5 from '@/assets/image/top/top_5.png';
-import top_6 from '@/assets/image/top/top_6.png';
-import top_7 from '@/assets/image/top/top_7.png';
+import top_1 from '@/assets/image/top/top_1.svg'
+import top_2 from '@/assets/image/top/top_2.svg'
+import top_3 from '@/assets/image/top/top_3.svg'
+import top_4 from '@/assets/image/top/top_4.svg'
+import top_5 from '@/assets/image/top/top_5.svg'
 
-export const itemsData = {
+export const MAIN_CATEGORY = {
+  FACE: 'face',
+  CLOTHES: 'clothes',
+  DECORATION: 'decoration',
+} as const
+
+export type MainCategoryKey = (typeof MAIN_CATEGORY)[keyof typeof MAIN_CATEGORY]
+
+export const SELECTABLE_SUB_CATEGORY = {
+  BODY: 'body',
+  EYES: 'eyes',
+  MOUTH: 'mouth',
+  HAIR: 'hair',
+  BLUSH: 'blush',
+  HAT: 'hat',
+  TOP: 'top',
+  PANTS: 'pants',
+  ONE_PIECE: 'onePiece',
+  ACCESSORY: 'accessory',
+} as const
+
+export const MAIN_CATEGORY_SUBS = {
+  [MAIN_CATEGORY.FACE]: [
+    SELECTABLE_SUB_CATEGORY.BODY,
+    SELECTABLE_SUB_CATEGORY.EYES,
+    SELECTABLE_SUB_CATEGORY.MOUTH,
+    SELECTABLE_SUB_CATEGORY.HAIR,
+    SELECTABLE_SUB_CATEGORY.BLUSH,
+  ],
+  [MAIN_CATEGORY.CLOTHES]: [
+    SELECTABLE_SUB_CATEGORY.HAT,
+    SELECTABLE_SUB_CATEGORY.TOP,
+    SELECTABLE_SUB_CATEGORY.PANTS,
+    SELECTABLE_SUB_CATEGORY.ONE_PIECE,
+  ],
+  [MAIN_CATEGORY.DECORATION]: [SELECTABLE_SUB_CATEGORY.ACCESSORY],
+} as const
+
+export type SelectableSubCategoryKey =
+  (typeof SELECTABLE_SUB_CATEGORY)[keyof typeof SELECTABLE_SUB_CATEGORY]
+
+export type SubCategoriesOf<M extends MainCategoryKey> =
+  (typeof MAIN_CATEGORY_SUBS)[M][number]
+
+export interface Item {
+  name: string
+  asset: string
+}
+
+export type SubCategory = {
+  type: string
+  data: Item[]
+}
+
+export type SelectedItems = {
+  [key in SelectableSubCategoryKey]: string | null
+}
+
+type SubCategoryRecord<M extends MainCategoryKey> = {
+  [S in SubCategoriesOf<M>]: SubCategory
+}
+
+export type ItemsData = {
+  [M in MainCategoryKey]: {
+    type: string
+  } & SubCategoryRecord<M>
+}
+
+export const itemsData: ItemsData = {
   face: {
-    body: [
-      { name: 'body_1', asset: body_1 },
-      { name: 'body_2', asset: body_2 },
-      { name: 'body_3', asset: body_3 },
-      { name: 'body_4', asset: body_4 },
-      { name: 'body_5', asset: body_5 },
-      { name: 'body_6', asset: body_6 },
-      { name: 'body_7', asset: body_7 },
-      { name: 'body_8', asset: body_8 },
-      { name: 'body_9', asset: body_9 },
-      { name: 'body_10', asset: body_10 },
-    ],
-    eyes: [
+    type: '얼굴',
+    body: {
+      type: '몸',
+      data: [
+        { name: 'body_1', asset: body_1 },
+        { name: 'body_2', asset: body_2 },
+        { name: 'body_3', asset: body_3 },
+        { name: 'body_4', asset: body_4 },
+        { name: 'body_5', asset: body_5 },
+        { name: 'body_6', asset: body_6 },
+        { name: 'body_7', asset: body_7 },
+        { name: 'body_8', asset: body_8 },
+        { name: 'body_9', asset: body_9 },
+        { name: 'body_10', asset: body_10 },
+      ],
+    },
+    eyes: {
+      type: '눈',
+      data: [
         { name: 'eye_1', asset: eye_1 },
         { name: 'eye_2', asset: eye_2 },
         { name: 'eye_3', asset: eye_3 },
@@ -130,8 +203,11 @@ export const itemsData = {
         { name: 'eye_13', asset: eye_13 },
         { name: 'eye_14', asset: eye_14 },
         { name: 'eye_15', asset: eye_15 },
-    ],
-    hair: [
+      ],
+    },
+    hair: {
+      type: '머리',
+      data: [
         { name: 'hair_1', asset: hair_1 },
         { name: 'hair_2', asset: hair_2 },
         { name: 'hair_3', asset: hair_3 },
@@ -139,8 +215,11 @@ export const itemsData = {
         { name: 'hair_5', asset: hair_5 },
         { name: 'hair_6', asset: hair_6 },
         { name: 'hair_7', asset: hair_7 },
-    ],
-    mouth: [
+      ],
+    },
+    mouth: {
+      type: '입',
+      data: [
         { name: 'mouth_1', asset: mouth_1 },
         { name: 'mouth_2', asset: mouth_2 },
         { name: 'mouth_3', asset: mouth_3 },
@@ -161,17 +240,24 @@ export const itemsData = {
         { name: 'mouth_18', asset: mouth_18 },
         { name: 'mouth_19', asset: mouth_19 },
         { name: 'mouth_20', asset: mouth_20 },
-    ],
-    blush: [
+      ],
+    },
+    blush: {
+      type: '홍조',
+      data: [
         { name: 'blush_1', asset: blush_1 },
         { name: 'blush_2', asset: blush_2 },
         { name: 'blush_3', asset: blush_3 },
         { name: 'blush_4', asset: blush_4 },
         { name: 'blush_5', asset: blush_5 },
-    ],
+      ],
+    },
   },
   clothes: {
-    hat: [
+    type: '옷',
+    hat: {
+      type: '모자',
+      data: [
         { name: 'hat_1', asset: hat_1 },
         { name: 'hat_2', asset: hat_2 },
         { name: 'hat_3', asset: hat_3 },
@@ -180,28 +266,46 @@ export const itemsData = {
         { name: 'hat_6', asset: hat_6 },
         { name: 'hat_7', asset: hat_7 },
         { name: 'hat_8', asset: hat_8 },
-    ],
-    onePiece: [
+      ],
+    },
+    onePiece: {
+      type: '원피스',
+      data: [
         { name: 'onePiece_1', asset: onePiece_1 },
         { name: 'onePiece_2', asset: onePiece_2 },
         { name: 'onePiece_3', asset: onePiece_3 },
         { name: 'onePiece_4', asset: onePiece_4 },
-    ],
-    pants: [
+      ],
+    },
+    pants: {
+      type: '하의',
+      data: [
         { name: 'pants_1', asset: pants_1 },
-        { name: 'pants_1_1', asset: pants_1_1 },
-        { name: 'pants_1_2', asset: pants_1_2 },
+        { name: 'pants_2', asset: pants_2 },
+        { name: 'pants_3', asset: pants_3 },
         { name: 'pants_4', asset: pants_4 },
         { name: 'pants_5', asset: pants_5 },
-    ],
-    top: [
+      ],
+    },
+    top: {
+      type: '상의',
+      data: [
         { name: 'top_1', asset: top_1 },
         { name: 'top_2', asset: top_2 },
         { name: 'top_3', asset: top_3 },
         { name: 'top_4', asset: top_4 },
         { name: 'top_5', asset: top_5 },
-        { name: 'top_6', asset: top_6 },
-        { name: 'top_7', asset: top_7 },
-    ],
+      ],
+    },
   },
-};
+  decoration: {
+    type: '장식',
+    accessory: {
+      type: '악세서리',
+      data: [
+        { name: 'accessory_1', asset: accessory_1 },
+        { name: 'accessory_2', asset: accessory_2 },
+      ],
+    },
+  },
+}
