96e6c40f9d0176995353cb0203db0491ff7fcda2
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2025-12-11 22:34:42 +0900

    feat: 보낸 쿠키 리스트 route 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -8,6 +8,8 @@ import Step1MakeCookie from '@/pages/cookie/Step1MakeCookie'
 import Step2ChooseReceiver from '@/pages/cookie/Step2ChooseReceiver'
 import Step3WriteLetter from '@/pages/cookie/Step3WriteLetter'
 import Home from '@/pages/Home'
+import Layout from '@/components/Layout'
+import SendList from '@/pages/cookie/SendList'
 
 const Router = () => {
   return (
@@ -15,7 +17,10 @@ const Router = () => {
       <Routes>
         <Route path="/landing" element={<LandingPage />} />
         {/* 구글 로그인 진입 로딩 */}
-        <Route path="/api/v1/auth/google/login" element={<GoogleConnectingPage />} />
+        <Route
+          path="/api/v1/auth/google/login"
+          element={<GoogleConnectingPage />}
+        />
         {/* ✅ 백엔드에서 리다이렉트해 주는 콜백 URL */}
         <Route path="/auth/callback" element={<GoogleConnectingPage />} />
         {/* SSAFY 인증 페이지 */}
@@ -23,15 +28,21 @@ const Router = () => {
         {/* 내 오븐 페이지 */}
         <Route path="/myoven" element={<MyOvenPage />} />
 
-        <Route path="/" element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="/cookie">
-          {/* /cookie로 들어오면 /cookie/step1로 리다이렉트 */}
-          <Route index element={<Navigate to="step1" replace />} />
-          <Route path="step1" element={<Step1MakeCookie />} />
-          <Route path="step2" element={<Step2ChooseReceiver />} />
-          <Route path="step3" element={<Step3WriteLetter />} />
-          <Route path="finish" element={<Finish />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/myoven" element={<MyOvenPage />} />
+
+          {/* cookie 관련은 Layout 내부에 중첩 */}
+          <Route path="/cookie">
+            <Route index element={<Navigate to="step1" replace />} />
+            <Route path="step1" element={<Step1MakeCookie />} />
+            <Route path="step2" element={<Step2ChooseReceiver />} />
+            <Route path="step3" element={<Step3WriteLetter />} />
+            <Route path="finish" element={<Finish />} />
+            <Route path="send" element={<SendList />} />
+            {/* <Route path="/:id" element={<CookieDetailPage />} /> */}
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
