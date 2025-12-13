7bdff074a715eb8681fecd3878f7fe358b78df5e
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2025-12-14 04:44:58 +0900

    feat: useUser

---
+++ src/hooks/queries/useUser.ts
@@ -0,0 +1,29 @@
+import { getUserApi } from '@/services/user'
+import type { User, UserResponse } from '@/types/user'
+import { useQuery } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
+
+export function useUser() {
+  const userQuery = useQuery<UserResponse, AxiosError, User>({
+    queryFn: () => getUserApi(),
+    queryKey: ['user', 'me'],
+    select: (data) => {
+      const mapUserResponse = (res: UserResponse): User => ({
+        userPk: res.user_pk,
+        googleEmail: res.google_email,
+        campus: res.campus,
+        classNumber: res.class_number,
+        name: res.name,
+        mmId: res.mm_id,
+        role: res.role as User['role'],
+        isVerified: res.is_verified,
+        isOvenOpen: res.is_oven_open,
+        isTutorialCompleted: res.is_tutorial_completed,
+        description: res.description,
+      })
+      return mapUserResponse(data)
+    },
+  })
+
+  return userQuery
+}
