debaacf103a08c39391552edf062950acd668acf
03hoho03 <122659293+03hoho03@users.noreply.github.com>
2025-12-13 19:08:23 +0900

    feat: reactquery provider

---
+++ src/lib/ReactQueryProvider.tsx
@@ -0,0 +1,35 @@
+// src/providers/ReactQueryProvider.tsx
+import { type ReactNode, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+interface Props {
+  children: ReactNode
+}
+
+export default function ReactQueryProvider({ children }: Props) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            refetchOnWindowFocus: false,
+            staleTime: 1000 * 60,
+          },
+          mutations: {
+            retry: 0,
+          },
+        },
+      }),
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
+  )
+}
